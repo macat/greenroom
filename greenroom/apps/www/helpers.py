ee3503c100180dac24ce3f2464ae1ae6b6d37a4b
virtuallight <mat.jankowski@gmail.com>
2012-08-19 17:33:08 +0200

    all fb friends

--- greenroom/apps/www/helpers.py
+++ greenroom/apps/www/helpers.py
@@ -2,11 +2,9 @@ from django_facebook.api import FacebookUserConverter, get_persistent_graph
 from open_facebook.api import FacebookAuthorization
 
 
-FB_FRIENDS_PAGINATION_LIMIT = 5
-
 def get_facebook_friends(request):
     app_access_token = FacebookAuthorization.get_cached_app_access_token()
     graph = get_persistent_graph(request, access_token=app_access_token)
     converter = FacebookUserConverter(graph)
-    friends_uids = ','.join([str(f['uid']) for f in converter.get_friends()][:FB_FRIENDS_PAGINATION_LIMIT])
+    friends_uids = ','.join([str(f['uid']) for f in converter.get_friends()])
     return graph.fql('SELECT name, username, pic_square, pic FROM user WHERE uid IN (%s)' % friends_uids)
