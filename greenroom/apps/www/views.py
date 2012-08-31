608bc57a47c0986de796369125b16702f0837f95
Antonella Tezza <antonellatezza@gmail.com>
2012-08-31 18:51:07 +0200

    add outfits in homepage

--- greenroom/apps/www/views.py
+++ greenroom/apps/www/views.py
@@ -6,4 +6,7 @@ from .helpers import get_facebook_friends
 def home(request):
     # user might be authenticated and logged in only via Facebook (no other auth possibility)
     fb_friends = get_facebook_friends(request) if request.user.is_authenticated() else []
-    return render(request, 'home.html', {'fb_friends': fb_friends})
+    return render(request, 'home.html', {'fb_friends': fb_friends,
+    	'outfits': request.user.outfit_set.all().order_by('-submitted_at') if request.user.is_authenticated() else []})
+    				
+
